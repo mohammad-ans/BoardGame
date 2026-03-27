0b4ac04078dcf778e26c029341b160286715e25e
mohammad-ans <ansmuhammad340@gmail.com>
2026-03-27 05:33:50 +0500

    changed the dice color, created a grid layout to represent the area and created a simple moving animation that creates a moving effect by moving between cells by color changing

--- app/src/main/java/com/example/boardgame/MainActivity.kt
+++ app/src/main/java/com/example/boardgame/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.boardgame
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.GridLayout
+import android.widget.GridView
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -15,9 +17,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import android.graphics.Color
+import android.view.Gravity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.boardgame.ui.theme.BoardGameTheme
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -29,6 +34,9 @@ class MainActivity : ComponentActivity() {
     lateinit var player1 : TextView
     lateinit var player2 : TextView
     lateinit var turn : TextView
+    lateinit var tiles : ArrayList<TextView>
+    lateinit var grid : GridLayout
+    lateinit var animationLaunch : Job
     var player1Pos = 0
     var player2Pos = 0
     var winningPoints = 50
@@ -43,7 +51,26 @@ class MainActivity : ComponentActivity() {
         player1 = findViewById<TextView>(R.id.player1)
         player2 = findViewById<TextView>(R.id.player2)
         turn = findViewById<TextView>(R.id.player_turn)
+        grid = findViewById<GridLayout>(R.id.grid)
+//        grid.layoutParams.height = resources.displayMetrics.heightPixels / 2
+        tiles = ArrayList()
 
+        val padding = (8 * resources.displayMetrics.density).toInt()
+        val sizeTile = resources.displayMetrics.widthPixels / 5
+        for(i in 50 downTo 1) {
+            val temp = TextView(this)
+            temp.text = "$i"
+            temp.textSize = 20f
+            temp.gravity = Gravity.CENTER
+            val params = GridLayout.LayoutParams()
+            params.width=sizeTile
+            params.height=(sizeTile * 2) / 3
+            temp.setBackgroundColor(Color.LTGRAY)
+            temp.layoutParams = params
+            grid.addView(temp)
+            tiles.add(0, temp)
+
+        }
         resetBtn.setOnClickListener(::resetGame)
         rollDice.setOnClickListener(::diceHandler)
     }
@@ -82,20 +109,43 @@ class MainActivity : ComponentActivity() {
         }
         diceImg.setImageResource(imgSrc)
         if(player1Turn) {
+            val start = player1Pos - 1
             player1Pos += diceVal
             player1.text = getString(R.string.player_1_dynamic, player1Pos)
+            mainAnimation(start, if (player1Pos <= 50) player1Pos else 49) {
+                tiles[if ((player1Pos - 1) < 50) player1Pos - 1 else 49].setBackgroundColor(Color.BLUE)
+                if(player1Pos > 1){
+                    tiles[if ((player1Pos - 2) < 49) player1Pos - 2 else 48].setBackgroundColor(Color.LTGRAY)
+                }
+            }
+
             if(player1Pos >= winningPoints) {
+                resetTiles()
+                tiles[49].setBackgroundColor(Color.BLUE)
                 winner("Player 1 wins")
+                animationLaunch.cancel()
                 return
             }
             player1Turn = !player1Turn
             turn.text = getString(R.string.player_turn_dynamic, 1)
         }
         else{
+            val start = player2Pos
             player2Pos += diceVal
             player2.text = getString(R.string.player_2_dynamic, player2Pos)
 
+
+            mainAnimation(start, if (player2Pos <= 50) player2Pos else 49) {
+                tiles[if ((player2Pos - 1) < 50) (player2Pos - 1) else 49].setBackgroundColor(Color.RED)
+                if(player2Pos > 1){
+                    tiles[if ((player2Pos - 2) < 49) (player2Pos - 2) else 48].setBackgroundColor(Color.LTGRAY)
+                }
+            }
+
             if(player2Pos >= winningPoints) {
+                animationLaunch.cancel()
+                resetTiles()
+                tiles[49].setBackgroundColor(Color.RED)
                 winner("Player 2 wins")
                 return
             }
@@ -116,6 +166,28 @@ class MainActivity : ComponentActivity() {
         turn.text = getString(R.string.player_turn)
         diceImg.setImageResource(R.drawable.dice_one)
 
+        resetTiles()
         rollDice.isEnabled = true
+    }
+    fun animation1(pos : Int) {
+        resetTiles()
+        if(pos > 0) {
+            tiles[pos - 1].setBackgroundColor(Color.YELLOW)
+        }
+    }
+    fun mainAnimation(start : Int, end : Int, func : () -> Unit) {
+
+        animationLaunch = lifecycleScope.launch {
+            for(pos in start..(end - 1)) {
+                animation1(pos)
+                delay(150)
+            }
+            func()
+        }
+    }
+    fun resetTiles() {
+        for (tile in tiles) {
+            tile.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }

f1b60a1500ceff4f22ad6ef198cee8baa20ee043
mohammad-ans <ansmuhammad340@gmail.com>
2026-07-22 03:27:06 +0500

    Voice connection endpoint fuctions for the online game developed, but testing not done

--- app/src/main/java/com/example/boardgame/OnlineGameConnection.kt
+++ app/src/main/java/com/example/boardgame/OnlineGameConnection.kt
@@ -3,6 +3,7 @@ package com.example.boardgame
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
+import android.media.AudioTrack
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
@@ -13,6 +14,17 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import org.json.JSONObject
+import org.webrtc.AudioSource
+import org.webrtc.DataChannel
+import org.webrtc.EglBase
+import org.webrtc.IceCandidate
+import org.webrtc.MediaConstraints
+import org.webrtc.MediaStream
+import org.webrtc.PeerConnection
+import org.webrtc.PeerConnectionFactory
+import org.webrtc.RtpReceiver
+import org.webrtc.SdpObserver
+import org.webrtc.SessionDescription
 import java.util.concurrent.TimeUnit
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
@@ -30,6 +42,19 @@ class OnlineGameConnection(private val context: Context, private val playerName:
     private var onError: ((String) -> Unit)? = null
     private var onConnectionFailed : (() -> Unit)? = null
     private val prefs = context.getSharedPreferences("game_prefs", Context.MODE_PRIVATE)
+    private var isInitiator = false
+    private val eglBase = EglBase.create()
+    private val peerConnectionFactory: PeerConnectionFactory by lazy {
+        PeerConnectionFactory.initialize(
+            PeerConnectionFactory.InitializationOptions.builder(context).createInitializationOptions()
+        )
+        PeerConnectionFactory.builder().createPeerConnectionFactory()
+    }
+    private var peerConnection: PeerConnection? = null
+    private var localAudioTrack: org.webrtc.AudioTrack? = null
+    private var audioSource: AudioSource? = null
+
+    private val iceServers = listOf(PeerConnection.IceServer.builder("stun:stun.l.google.com").createIceServer() )
     @OptIn(ExperimentalUuidApi::class)
     private val username = Uuid.random().toString()
     override fun onMoveReceived(callback: (GameMove) -> Unit) {
@@ -86,6 +111,7 @@ class OnlineGameConnection(private val context: Context, private val playerName:
             "waiting_for_match" -> onWaitingForMatch?.invoke()
             "matched" -> {
                 currentRoomCode = json.getString("room_code")
+                isInitiator = json.getBoolean("is_initiator")
                 onMatched?.invoke(currentRoomCode!!)
             }
             "player_joined" -> {
@@ -104,12 +130,102 @@ class OnlineGameConnection(private val context: Context, private val playerName:
             "move" -> {
                 moveCallback?.invoke(GameMove(playerId = "Player", diceVal = json.getInt("diceVal")))
             }
+            "voice_offer" -> {
+                val sdp = SessionDescription(SessionDescription.Type.ANSWER, json.getString("sdp"))
+                peerConnection?.setRemoteDescription(SimpleSdpObserver(), sdp)
+
+            }
+            "voice_answer" -> {
+                val sdp = SessionDescription(SessionDescription.Type.OFFER, json.getString("sdp"))
+                peerConnection?.setRemoteDescription(SimpleSdpObserver(), sdp)
+            }
+            "voice_ice_candidate" -> {
+                val candidate = IceCandidate(
+                    json.getString("sdpMid"),
+                    json.getInt("sdpMLineIndex"),
+                    json.getString("candidate")
+                )
+                peerConnection?.addIceCandidate(candidate)
+            }
             "username" -> prefs.edit().putString("username2", json.getString("username"))
             "opponent_disconnected" -> onOpponentsDisconnected?.invoke()
             "error" -> onError?.invoke(json.optString("message", "Server Error"))
         }
     }
+    override fun startVoiceChat() {
+        val constraints = MediaConstraints()
+        audioSource = peerConnectionFactory.createAudioSource(constraints)
+        localAudioTrack = peerConnectionFactory.createAudioTrack("audio_track", audioSource)
+        val rtcConfig = PeerConnection.RTCConfiguration(iceServers)
+        peerConnection = peerConnectionFactory.createPeerConnection(rtcConfig, object : PeerConnection.Observer {
+            override fun onIceCandidate(candidate: IceCandidate?) {
+                send(JSONObject().apply {
+                    put("type", "voice_ice_candidate")
+                    put("room_code", currentRoomCode)
+                    put("candidate", candidate?.sdp)
+                    put("sdpMid", candidate?.sdpMid)
+                    put("sdpMLineIndex", candidate?.sdpMLineIndex)
+                })
+            }
 
+            override fun onIceConnectionChange(state: PeerConnection.IceConnectionState?) {
+                Log.d("WebRTC", "Ice State: $state")
+            }
+
+            override fun onAddStream(p0: MediaStream?) {
+
+            }
+
+            override fun onSignalingChange(p0: PeerConnection.SignalingState?) {
+
+            }
+
+            override fun onIceConnectionReceivingChange(p0: Boolean) {
+
+            }
+
+            override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
+
+            }
+
+            override fun onRemoveStream(p0: MediaStream?) {
+
+            }
+
+            override fun onDataChannel(p0: DataChannel?) {
+
+            }
+
+            override fun onAddTrack(
+                receiver: RtpReceiver?,
+                mediaStreams: Array<out MediaStream?>?
+            ) {
+            }
+
+            override fun onRenegotiationNeeded() {
+            }
+
+            override fun onIceCandidatesRemoved(p0: Array<out IceCandidate?>?) {
+
+            }
+        })
+        peerConnection?.addTrack(localAudioTrack, listOf("stream1"))
+        if (isInitiator)
+            createAndSendOffer()
+    }
+    private fun createAndSendOffer(){
+        val constraints = MediaConstraints()
+        peerConnection?.createAnswer(object : SimpleSdpObserver() {
+            override fun onCreateSuccess(sdp: SessionDescription?) {
+                peerConnection?.setLocalDescription(SimpleSdpObserver(), sdp)
+                send(JSONObject().apply {
+                    put("type", "voice_answer")
+                    put("room_code", currentRoomCode)
+                    put("sdp", sdp?.description)
+                })
+            }
+        }, constraints)
+    }
     private fun send(json : JSONObject) {
         Log.e("Move", "Sending move")
         webSocket?.send(json.toString())
@@ -174,5 +290,27 @@ class OnlineGameConnection(private val context: Context, private val playerName:
         onOpponentDisconnected: () -> Unit
     ) {
         TODO("Not yet implemented")
+    }
+
+    override fun setMuted(muted: Boolean) {
+        localAudioTrack?.setEnabled(!muted)
+    }
+
+    override fun stopVoiceChat() {
+        peerConnection?.close()
+        peerConnection = null
+        localAudioTrack = null
+        audioSource = null
+    }
+    open class SimpleSdpObserver : SdpObserver {
+        override fun onCreateSuccess(sdp: SessionDescription?) {}
+        override fun onSetSuccess() {}
+        override fun onCreateFailure(error: String?) {
+            Log.e("WebRTC", "Create failure: $error")
+        }
+
+        override fun onSetFailure(error: String?) {
+            Log.e("WebRTC", "Set failure: $error")
+        }
     }
 }

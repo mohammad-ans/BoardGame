315f469fe4f38e3ab5aaa6cf0d0e953db7be8fa9
mohammad-ans <ansmuhammad340@gmail.com>
2026-08-02 21:02:44 +0500

    Integrated the fetch user details function to display their stats and save those locally so if they open later but being offline then the saved old stats are shown to them

--- app/src/main/java/com/example/boardgame/Profile.kt
+++ app/src/main/java/com/example/boardgame/Profile.kt
@@ -1,5 +1,7 @@
 package com.example.boardgame
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,7 @@ import com.example.boardgame.databinding.ProfileBinding
 
 class ProfileFragment: Fragment(R.layout.profile) {
     lateinit var binding: ProfileBinding
+    private lateinit var prefs: SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,8 +23,32 @@ class ProfileFragment: Fragment(R.layout.profile) {
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        prefs = requireContext().getSharedPreferences("game_prefs", Context.MODE_PRIVATE)
+        val api = Api("https://yappyyap.xyz")
+        val uuid = prefs.getString("uuid", null)!!
+        api.fetchProfile(username = uuid, ::f){
+            requireActivity().runOnUiThread {
+                val wins = prefs.getInt("wins", 0)
+                val losses = prefs.getInt("losses", 0)
+                binding.wins.text = getString(R.string.wins, wins)
+                binding.losses.text = getString(R.string.loss, losses)
+            }
+        }
+        val username = prefs.getString("username", null)
+        binding.profileHeading.text = username
+
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+    fun f(profile: ProfileResult?) {
+        requireActivity().runOnUiThread {
+            binding.wins.text = getString(R.string.wins, profile?.wins)
+            binding.losses.text = getString(R.string.loss, profile?.losses)
+            prefs.edit().apply {
+                putInt("wins", profile?.wins ?: 0)
+                putInt("losses", profile?.losses ?: 0)
+            }
         }
     }
 }
